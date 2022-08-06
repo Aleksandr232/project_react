fb10ad2a1174f0205a78d3a27adda603bed75aad
Aleksandr232 <aleksrowing232@gmail.com>
2022-08-06 18:34:37 +0300

    red

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ function App() {
   return (
     <div className="container mx-auto max-w-2xl pt-5">
       {loading &&<p className="text-center">Загрузка....</p>}
-      {error &&<p className="text-center">{error}</p>}
+      {error &&<p className="text-center text-red-600">{error}</p>}
       {products.map(product => <Product product={product} key={product.id}/>)}
     </div>
     
