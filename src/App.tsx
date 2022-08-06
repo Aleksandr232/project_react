fd5d7cc3956204875aae80e4fb0cf84044c7c17a
Aleksandr232 <aleksrowing232@gmail.com>
2022-08-06 18:04:32 +0300

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Product } from './components/Product';
 import { products } from './data/product';
+import { IProduct } from './module';
 
 
 function App() {
   return (
     <div className="container mx-auto max-w-2xl pt-5">
-      {products.map(product=> <Product product={product}/>)}
+      {products.map((product:IProduct, index:number) => <Product product={product} key={product.id}/>)}
     </div>
     
   );
