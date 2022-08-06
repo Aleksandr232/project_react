8b762e92a0b71f301fc5a394fb64b41745b065e9
Aleksandr232 <aleksrowing232@gmail.com>
2022-08-06 16:46:15 +0300

    product

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import { Product } from './components/Product';
+import { products } from './data/product';
 
 
 function App() {
   return (
-    <div>ewf</div>
+    <div className="container mx-auto max-w-2xl pt-5">
+        <Product product={products[0]}
+        
+        />
+    </div>
     
   );
 }
