e9303a86387cc5466f07dab5742b1c3fc50b5b90
Aleksandr232 <aleksrowing232@gmail.com>
2022-08-06 18:09:57 +0300

    axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Product } from './components/Product';
 import { products } from './data/product';
 import { IProduct } from './module';
 
 
 function App() {
+
+  useEffect(()=>{
+    
+  },[])
+
   return (
     <div className="container mx-auto max-w-2xl pt-5">
       {products.map(product => <Product product={product} key={product.id}/>)}
