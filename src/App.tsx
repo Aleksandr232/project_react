785c40230970ef9886fc870676c7c4499839401d
Aleksandr232 <aleksrowing232@gmail.com>
2022-08-07 14:08:41 +0300

    useProducts

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,10 @@
-import axios, { Axios, AxiosError } from 'axios';
-import React, {useState, useEffect } from 'react';
+import { useProducts } from './hooks/products';
 import { Product } from './components/Product';
-import { IProduct } from './module';
+
 
 
 function App() {
-  const [products, setProducts] = useState<IProduct[]>([])
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState('')
-  async function fetchProducts() {
-    try{
-      setError('')
-      setLoading(true)
-      const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=10')
-      setProducts(response.data)
-      setLoading(false)
-    }catch(e: unknown){
-      const error = e as AxiosError
-      setLoading(false)
-      setError(error.message)
-    }
-   
-    
-  }
-
-  useEffect(()=>{
-    fetchProducts()
-  },[])
-
+  const {loading, error, products} = useProducts()
   return (
     <div className="container mx-auto max-w-2xl pt-5">
       {loading &&<p className="text-center">Загрузка....</p>}
--- src/hooks/products.ts
+++ src/hooks/products.ts
@@ -1,3 +1,30 @@
-export function useProducts(){
+import React,{useState, useEffect} from "react"
+import axios, { Axios, AxiosError } from 'axios';
+import { IProduct } from "../module"
 
+export function useProducts(){
+    const [products, setProducts] = useState<IProduct[]>([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState('')
+  async function fetchProducts() {
+    try{
+      setError('')
+      setLoading(true)
+      const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=10')
+      setProducts(response.data)
+      setLoading(false)
+    }catch(e: unknown){
+      const error = e as AxiosError
+      setLoading(false)
+      setError(error.message)
+    }
+   
+    
+  }
+
+  useEffect(()=>{
+    fetchProducts()
+  },[])
+
+  return {products, error, loading}
 }
